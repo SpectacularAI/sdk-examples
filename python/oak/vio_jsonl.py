66064302a93edbddac0a3c8bff7fccee05186b47
Jerry Ylilammi <jerry.ylilammi@gmail.com>
2021-11-04 11:26:12 +0200

    Python API recording example

--- python/oak/vio_jsonl.py
+++ python/oak/vio_jsonl.py
@@ -6,6 +6,8 @@ pipeline = depthai.Pipeline()
 
 vio_pipeline = spectacularAI.depthai.Pipeline(pipeline, useStereo=False)
 # optional config args: vio_pipeline = spectacularAI.depthai.Pipeline(pipeline, config, useStereo=False)
+# for example enable recording:
+# vio_pipeline = spectacularAI.depthai.Pipeline(pipeline, recordingFolder="myfolder")
 
 with depthai.Device(pipeline) as device, \
     vio_pipeline.startSession(device) as vio_session:
