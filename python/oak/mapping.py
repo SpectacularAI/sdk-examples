5cc849a92e2cbd8872eef781df506a602a9a4e4a
Jerry Ylilammi <jerry.ylilammi@gmail.com>
2022-06-03 15:08:24 +0300

    Added mapping example using realtime data from OAK-D device

--- python/oak/mapping.py
+++ python/oak/mapping.py
@@ -3,16 +3,18 @@ import spectacularAI
 import cv2
 import json
 import os
+import depthai
 
 p = argparse.ArgumentParser(__doc__)
-p.add_argument("dataFolder", help="Folder containing the recorded session for mapping", default="data")
-p.add_argument("outputFolder", help="Output folder for key frame images and their poses ", default="output")
+p.add_argument("--dataFolder", help="Folder containing the recorded session for mapping")
+p.add_argument("--outputFolder", help="Output folder for key frame images and their poses ", default="output")
 p.add_argument("--preview", help="Show latest primary image as a preview", action="store_true")
 args =  p.parse_args()
 
 # KeyFrames for which we've already saved the image
 savedKeyFrames = {}
 
+running = True
 
 def saveAsPng(outputFolder, frameId, cameraName, frame):
     if not frame or not frame.image: return
@@ -21,11 +23,13 @@ def saveAsPng(outputFolder, frameId, cameraName, frame):
 
 
 def onMappingOutput(output):
+    global running
+    global savedKeyFrames
     if output.finalMap:
         # Final optimized poses, let's save them to jsonl file
         with open(args.outputFolder + "/poses.jsonl", "w") as outFile:
-            for frameId in output.map.keyframes:
-                keyFrame = output.map.keyframes.get(frameId)
+            for frameId in output.map.keyFrames:
+                keyFrame = output.map.keyFrames.get(frameId)
                 outputJson = {
                     "frameId": frameId,
                     "poses": {}
@@ -38,22 +42,37 @@ def onMappingOutput(output):
                 outFile.write(json.dumps(outputJson) + "\n")
     else:
         # New frames, let's save the images to disk
-        for frameId in output.updatedKeyframes:
-            keyFrame = output.map.keyframes.get(frameId)
-            if not keyFrame or savedKeyFrames.get(keyFrame):
-                continue
+        for frameId in output.updatedKeyFrames:
+            keyFrame = output.map.keyFrames.get(frameId)
+            if not keyFrame or savedKeyFrames.get(keyFrame): continue
             savedKeyFrames[keyFrame] = True
             frameSet = keyFrame.frameSet
+            if not frameSet: continue
             saveAsPng(args.outputFolder, frameId, "primary", frameSet.primaryFrame)
             saveAsPng(args.outputFolder, frameId, "secondary", frameSet.secondaryFrame)
             saveAsPng(args.outputFolder, frameId, "rgb", frameSet.rgbFrame)
             saveAsPng(args.outputFolder, frameId, "depth", frameSet.depthFrame)
-            if args.preview and frameSet.primaryFrame:
+            if args.preview and frameSet.primaryFrame and frameSet.primaryFrame.image:
                 cv2.imshow("Primary camera", cv2.cvtColor(frameSet.primaryFrame.image.toArray(), cv2.COLOR_RGB2BGR))
-                cv2.setWindowTitle("Primary camera", "Primary camera #{}".format(frameId))
-                cv2.waitKey(1)
+                cv2.setWindowTitle("Primary camera", "Primary camera #{} (Q or ESC to exit)".format(frameId))
+                key = cv2.waitKey(1)
+                if key == 113 or key == 27: # q or ESC
+                    running = False
 
 
-os.makedirs(args.outputFolder)
-replay = spectacularAI.Replay(args.dataFolder, onMappingOutput)
-replay.startReplay()
+if not os.path.isdir(args.outputFolder): os.makedirs(args.outputFolder)
+
+if args.dataFolder:
+    print("Starting replay")
+    replay = spectacularAI.Replay(args.dataFolder, onMappingOutput)
+    replay.startReplay()
+else:
+    print("Starting OAK-D device")
+    pipeline = depthai.Pipeline()
+    config = spectacularAI.depthai.Configuration()
+    vio_pipeline = spectacularAI.depthai.Pipeline(pipeline, config, onMappingOutput)
+    with depthai.Device(pipeline) as device, \
+        vio_pipeline.startSession(device) as vio_session:
+        while running:
+            out = vio_session.waitForOutput()
+
