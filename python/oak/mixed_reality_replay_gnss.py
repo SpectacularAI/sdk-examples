cab1275d60d130f800d8fd59a1e93d94a6766a54
Jerry Ylilammi <jerry.ylilammi@gmail.com>
2022-06-15 17:33:08 +0300

    Mixed reality example with global positioning

--- python/oak/mixed_reality_replay_gnss.py
+++ python/oak/mixed_reality_replay_gnss.py
@@ -9,7 +9,6 @@ import spectacularAI
 import pygame
 import time
 import numpy as np
-# import init_display from mixed_reality
 from mixed_reality import init_display, draw_cube, draw, load_obj
 
 from OpenGL.GL import * # all prefixed with gl so OK to import *
@@ -20,16 +19,34 @@ def parse_args():
     p.add_argument("dataFolder", help="Folder containing the recorded session for replay", default="data")
     p.add_argument("--mapLoadPath", help="SLAM map path", default=None)
     p.add_argument('--objLoadPath', help="Load scene as .obj", default=None)
+    p.add_argument('--latitude', help="Objects latitude coordinate", default=None)
+    p.add_argument('--longitude', help="Objects longitude coordinate", default=None)
+    p.add_argument('--altitude', help="Objects altitude in meters", default=None)
     return p.parse_args()
 args = parse_args()
 
 display_initialized = False
 obj = None
+objPos = None
+if args.latitude and args.longitude and args.altitude:
+    objPos = spectacularAI.WgsCoordinates()
+    objPos.latitude = float(args.latitude)
+    objPos.longitude = float(args.longitude)
+    objPos.altitude = float(args.altitude)
 
 def onOutput(output, frameSet):
     global display_initialized
     global obj
+    global objPos
     global args
+    if not output.globalPose:
+        print("[{:.2f}] No GNSS pose yet, waiting for long enough trail to align Vio and GNSS tracks".format(output.pose.time))
+        return
+
+    if objPos == None:
+        # Place object at the first received device coordinates if not provide through CLI arguments
+        objPos = output.globalPose.coordinates
+
     for frame in frameSet:
         if frame.image.getColorFormat() == spectacularAI.ColorFormat.RGB:
             img = frame.image.toArray()
@@ -48,8 +65,12 @@ def onOutput(output, frameSet):
                     pygame.quit()
                     return
 
+            cameraIndex = 0
+            # Gives camera pose relative to objPos that sits at [0,0,0] in ENU
+            cameraPose = output.globalPose.getEnuCameraPose(cameraIndex, objPos)
+
             is_tracking = output.status == spectacularAI.TrackingStatus.TRACKING
-            draw(frame.cameraPose, width, height, img.data, obj, is_tracking)
+            draw(cameraPose, width, height, img.data, obj, is_tracking)
             pygame.display.flip()
             break # Skip other frames
 
