5b496d65f1412125720fc191cae61d3eb9ad65cc
Jerry Ylilammi <jerry.ylilammi@gmail.com>
2023-04-14 17:45:17 +0300

    Draw camera trajectory. Draw camera as a box.

--- python/oak/mapping_visu.py
+++ python/oak/mapping_visu.py
@@ -31,6 +31,26 @@ def invert_se3(a):
     b[:3, 3] = -np.dot(b[:3, :3], a[:3, 3])
     return b
 
+def blend(a, b, m):
+    return [a[0] * (1 - m) + b[0] * m, a[1] * (1 - m) + b[1] * m, a[2] * (1 - m) + b[2] * m]
+
+class Trajectory:
+    def __init__(self):
+        self.maxPoints = 1000
+        self.points = []
+        self.colors = []
+        self.cloud = o3d.geometry.PointCloud()
+        for i in range(self.maxPoints):
+            self.colors.append(blend([0.960, 0.0192, 0.270], [.217, .009, .9], i / (self.maxPoints - 1)))
+            self.points.append([0,0,0])
+        self.cloud.colors = o3d.utility.Vector3dVector(self.colors)
+
+    def addPosition(self, pos):
+        while (len(self.points) < self.maxPoints): self.points.append(pos)
+        self.points.insert(0, pos)
+        self.points = self.points[:self.maxPoints]
+        self.cloud.points = o3d.utility.Vector3dVector(self.points)
+
 # Wrapper around Open3D point cloud, which helps updating its world pose.
 class PointCloud:
     def __init__(self, keyFrame, voxelSize, colorOnly):
@@ -69,10 +89,21 @@ class PointCloud:
         self.cloud.transform(prevToCurrent)
         self.camToWorld = camToWorld
 
-# Wrapper around Open3D coordinate frame, which helps updating its world pose.
+# Camera object
 class CoordinateFrame:
     def __init__(self, scale=0.25):
-        self.frame = o3d.geometry.TriangleMesh.create_coordinate_frame(scale)
+        # self.frame = o3d.geometry.TriangleMesh.create_coordinate_frame(scale)
+        corners = np.array([[0., 0., 0.], [0., 1., 0.], [1., 1., 0.], [1., 0., 0.], [0., 0., 1.], [0., 1., 1.], [1., 1., 1.], [1., 0., 1.]])
+        vertices = np.array(list(map(lambda n: n - [.5, .5, 0] if n[2] == 1. else (n - [.5, .5, 0]) * .5, corners))) * .1
+        colors = np.array(list(map(lambda n: np.array([0.960, 0.0192, 0.270]) * (.7 + .3 * n[0]) * (1. - .3 * n[1]) * (.7 + .3 * n[2]), corners)))
+        quads = np.array([[0, 1, 2, 3], [0, 4, 5, 1], [1, 5, 6, 2], [2, 6, 7, 3], [0, 3, 7, 4], [4, 7, 6, 5]])
+        triangles = []
+        for quad in quads:
+            triangles.append([quad[0], quad[1], quad[2]])
+            triangles.append([quad[2], quad[3], quad[0]])
+        triangles = np.array(triangles)
+        self.frame = o3d.geometry.TriangleMesh(o3d.utility.Vector3dVector(vertices), o3d.utility.Vector3iVector(triangles))
+        self.frame.vertex_colors = o3d.utility.Vector3dVector(colors)
         self.camToWorld = np.identity(4)
 
     def updateWorldPose(self, camToWorld):
@@ -82,9 +113,10 @@ class CoordinateFrame:
         self.camToWorld = camToWorld
 
 class Open3DVisualization:
-    def __init__(self, voxelSize, cameraManual, cameraSmooth, colorOnly):
+    def __init__(self, voxelSize, cameraManual, cameraSmooth, colorOnly, trajectory=False):
         self.shouldClose = False
         self.cameraFrame = CoordinateFrame()
+        self.trajectory = Trajectory() if trajectory else None
         self.pointClouds = {}
         self.voxelSize = voxelSize
         self.cameraFollow = not cameraManual
@@ -96,6 +128,7 @@ class Open3DVisualization:
         self.vis = o3d.visualization.Visualizer()
         self.vis.create_window()
         self.vis.add_geometry(self.cameraFrame.frame, reset_bounding_box=False)
+        if self.trajectory: self.vis.add_geometry(self.trajectory.cloud)
         self.viewControl = self.vis.get_view_control()
         renderOption = self.vis.get_render_option()
         renderOption.point_size = 3
@@ -110,6 +143,9 @@ class Open3DVisualization:
 
             # Update camera coordinate axes
             self.vis.update_geometry(self.cameraFrame.frame)
+
+            # Update trajectory
+            if self.trajectory: self.vis.update_geometry(self.trajectory.cloud)
 
             # Update point clouds (add, move, remove)
             for pcId in list(self.pointClouds.keys()):
@@ -138,6 +174,8 @@ class Open3DVisualization:
 
     def updateCameraFrame(self, camToWorld):
         self.cameraFrame.updateWorldPose(camToWorld)
+
+        if self.trajectory: self.trajectory.addPosition(camToWorld[0:3, 3])
 
         if self.cameraFollow:
             pos = camToWorld[0:3, 3]
@@ -192,6 +230,7 @@ def parseArgs():
     p.add_argument("--smooth", help="Apply some smoothing to 3rd person camera movement", action="store_true")
     p.add_argument("--color", help="Filter points without color", action="store_true")
     p.add_argument("--use_rgb", help="Use OAK-D RGB camera", action="store_true")
+    p.add_argument("--trajectory", help="Draw camera trajectory", action="store_true")
     p.add_argument('--ir_dot_brightness', help='OAK-D Pro (W) IR laser projector brightness (mA), 0 - 1200', type=float, default=0)
     p.add_argument('--no_feature_tracker', help='Disable on-device feature tracking and depth map', action="store_true")
     p.add_argument("--useRectification", help="--dataFolder option can also be used with some non-OAK-D recordings, but this parameter must be set if the videos inputs are not rectified.", action="store_true")
@@ -213,7 +252,7 @@ if __name__ == '__main__':
         configInternal["alreadyRectified"] = "true"
 
     voxelSize = 0 if args.voxel is None else float(args.voxel)
-    visu3D = Open3DVisualization(voxelSize, args.manual, args.smooth, args.color)
+    visu3D = Open3DVisualization(voxelSize, args.manual, args.smooth, args.color, args.trajectory)
 
     def onVioOutput(vioOutput):
         cameraPose = vioOutput.getCameraPose(0)
