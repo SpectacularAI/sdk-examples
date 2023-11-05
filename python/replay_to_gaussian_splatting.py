05c34cdc62e6b0995232eab212c557b874c675fa
Otto Seiskari <otto.seiskari@gmail.com>
2023-11-05 17:37:04 +0200

    Support both Nerfstudio and Taichi output formats

--- python/replay_to_gaussian_splatting.py
+++ python/replay_to_gaussian_splatting.py
@@ -16,8 +16,8 @@ from scipy.spatial import KDTree
 
 parser = argparse.ArgumentParser()
 parser.add_argument("input", help="Path to folder with session to process")
-parser.add_argument("output", help="Output folder, this should be 'data/' folder under taichi_3d_gaussian_splatting")
-parser.add_argument("--name", help="Session name", default="splatting_test")
+parser.add_argument("output", help="Output folder")
+parser.add_argument('--format', choices=['taichi', 'nerfstudio'], default='taichi', help='Output format')
 parser.add_argument("--cell_size", help="Point cloud decimation cell size", type=float, default=0.025)
 parser.add_argument("--key_frame_distance", help="Minimum distance between keyframes (meters)", type=float, default=0.05)
 parser.add_argument('--device_preset', choices=['none', 'k4a', 'realsense'], default='none')
@@ -48,6 +48,48 @@ def voxel_decimate(df, cell_size):
     df['voxel_index'] = df.apply(grouping_function, axis=1)
     grouped = df.groupby('voxel_index')
     return grouped.first().reset_index()
+
+def convert_json_taichi_to_nerfstudio(d):
+    def transform_camera(c):
+        convention_change = np.array([
+            [1, 0, 0, 0],
+            [0,-1, 0, 0],
+            [0, 0,-1, 0],
+            [0, 0, 0, 1]
+        ])
+        return (np.array(c) @ convention_change).tolist()
+
+    by_camera = {}
+    for c in d:
+        k = c['camera_intrinsics']
+        params = {
+            "fl_x": k[0][0],
+            "fl_y": k[1][1],
+            "k1": 0,
+            "k2": 0,
+            "p1": 0,
+            "p2": 0,
+            "cx": k[0][2],
+            "cy": k[1][2],
+            "w": c['camera_width'],
+            "h": c['camera_height'],
+            "aabb_scale": 16,
+            'frames': []
+        }
+        cam_id = json.dumps(params, sort_keys=True)
+        if cam_id not in by_camera:
+            by_camera[cam_id] = params
+
+        by_camera[cam_id]['frames'].append({
+            'file_path':  "./images/" + c['image_path'].split('/')[-1],
+            "transform_matrix": transform_camera(c['T_pointcloud_camera'])
+        })
+
+    if len(by_camera) != 1:
+        raise RuntimeError("unexpected number of cameras")
+
+    key, value = list(by_camera.items())[0]
+    return value
 
 # Globals
 savedKeyFrames = {}
@@ -93,7 +135,7 @@ def onMappingOutput(output):
             if intrinsics is None: intrinsics = undistortedFrame.cameraPose.camera.getIntrinsicMatrix()
             img = undistortedFrame.image.toArray()
             bgrImage = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-            fileName = f"{args.output}/{args.name}/tmp/frame_{frameId:05}.png"
+            fileName = f"{args.output}/tmp/frame_{frameId:05}.png"
             cv2.imwrite(fileName, bgrImage)
             if args.preview:
                 cv2.imshow("Frame", bgrImage)
@@ -106,7 +148,7 @@ def onMappingOutput(output):
         blurryImages = {}
         imageSharpness = []
         for frameId in output.map.keyFrames:
-            imageSharpness.append((frameId, blurScore(f"{args.output}/{args.name}/tmp/frame_{frameId:05}.png")))
+            imageSharpness.append((frameId, blurScore(f"{args.output}/tmp/frame_{frameId:05}.png")))
 
         # Look two images forward and two backwards, if current frame is blurriest, don't use it
         for i in range(len(imageSharpness)):
@@ -121,19 +163,20 @@ def onMappingOutput(output):
         validationFrames = []
         globalPointCloud = []
         index = 0
+        name = os.path.split(args.output)[-1]
         for frameId in output.map.keyFrames:
             if blurryImages.get(frameId): continue # Skip blurry images
 
             # Image data
             keyFrame = output.map.keyFrames.get(frameId)
-            oldImgName = f"{args.output}/{args.name}/tmp/frame_{frameId:05}.png"
-            newImgName = f"{args.output}/{args.name}/images/frame_{index:05}.png"
+            oldImgName = f"{args.output}/tmp/frame_{frameId:05}.png"
+            newImgName = f"{args.output}/images/frame_{index:05}.png"
             os.rename(oldImgName, newImgName)
             cameraPose = keyFrame.frameSet.rgbFrame.cameraPose
 
             # Camera data
             frame = {
-                "image_path": f"data/{args.name}/images/frame_{index:05}.png",
+                "image_path": f"data/{name}/images/frame_{index:05}.png",
                 "T_pointcloud_camera": cameraPose.getCameraToWorldMatrix().tolist(), # 4x4 matrix, the transformation matrix from camera coordinate to point cloud coordinate
                 "camera_intrinsics": intrinsics.tolist(), # 3x3 matrix, the camera intrinsics matrix K
                 "camera_height": frameHeight, # image height, in pixel
@@ -156,28 +199,38 @@ def onMappingOutput(output):
 
         # Save files
         point_cloud_df = pd.DataFrame(np.array(globalPointCloud), columns=list('xyzrgb'))
-        # point_cloud_df.to_csv(f"{args.output}/{args.name}/points.dense.csv", index=False)
+        # point_cloud_df.to_csv(f"{args.output}/points.dense.csv", index=False)
 
         # drop uncolored points
         colored_point_cloud_df = point_cloud_df.loc[point_cloud_df[list('rgb')].max(axis=1) > 0].reset_index()
-        sparse_point_cloud_df = pd.read_csv(f"{args.output}/{args.name}/points.sparse.csv", usecols=list('xyz'))
+        sparse_point_cloud_df = pd.read_csv(f"{args.output}/points.sparse.csv", usecols=list('xyz'))
 
         sparse_colored_point_cloud_df = interpolate_missing_properties(colored_point_cloud_df, sparse_point_cloud_df)
         decimated_df = voxel_decimate(colored_point_cloud_df, args.cell_size)
         merged_df = pd.concat([sparse_colored_point_cloud_df, decimated_df])
-        # merged_df.to_csv(f"{args.output}/{args.name}/points.merged-decimated.csv", index=False)
-        merged_df.to_parquet(f"{args.output}/{args.name}/point_cloud.parquet")
 
-        with open(f"{args.output}/{args.name}/train.json", "w") as outFile:
-            json.dump(trainingFrames, outFile, indent=2)
+        if args.format == 'taichi':
+            # merged_df.to_csv(f"{args.output}/points.merged-decimated.csv", index=False)
+            merged_df.to_parquet(f"{args.output}/point_cloud.parquet")
 
-        with open(f"{args.output}/{args.name}/val.json", "w") as outFile:
-            json.dump(validationFrames, outFile, indent=2)
+            with open(f"{args.output}/train.json", "w") as outFile:
+                json.dump(trainingFrames, outFile, indent=2, sort_keys=True)
 
+            with open(f"{args.output}/val.json", "w") as outFile:
+                json.dump(validationFrames, outFile, indent=2, sort_keys=True)
+        elif args.format == 'nerfstudio':
+            allFrames = trainingFrames + validationFrames
+            with open(f"{args.output}/transforms.json", "w") as outFile:
+                json.dump(convert_json_taichi_to_nerfstudio(allFrames), outFile, indent=2, sort_keys=True)
+
+            # colmap text point format
+            fake_colmap = f"{args.output}/colmap/sparse/0"
+            os.makedirs(fake_colmap, exist_ok=True)
+            merged_df.to_csv(f"{fake_colmap}/points3D.txt", index=False, sep=' ', header=False)
 
 def main():
-    os.makedirs(f"{args.output}/{args.name}/images", exist_ok=True)
-    tmp_dir = f"{args.output}/{args.name}/tmp"
+    os.makedirs(f"{args.output}/images", exist_ok=True)
+    tmp_dir = f"{args.output}/tmp"
     tmp_input = f"{tmp_dir}/input"
     os.makedirs(tmp_input, exist_ok=True)
     shutil.rmtree(tmp_input)
@@ -201,7 +254,8 @@ def main():
         "maxKeypoints": 2000,
         "mergeMultiLevelPointsThresholdPixels": 1,
         "orbRelativeMaskRadius": 0.01,
-        "mapSavePath": f"{args.output}/{args.name}/points.sparse.csv"
+        "discardOutputsWhenFull": False,
+        "mapSavePath": f"{args.output}/points.sparse.csv"
     }
 
     if args.slow:
@@ -220,12 +274,16 @@ def main():
 
     print("Done!")
     print("")
-    print("You should use following paths in taichi_3d_gaussian_splatting config file:")
-    print(f"pointcloud-parquet-path: 'data/{args.name}/point_cloud.parquet'")
-    print(f"summary-writer-log-dir: data/{args.name}/logs")
-    print(f"output-model-dir: data/{args.name}/output")
-    print(f"train-dataset-json-path: 'data/{args.name}/train.json'")
-    print(f"val-dataset-json-path: 'data/{args.name}/val.json'")
+    if args.format == 'taichi':
+        name = os.path.split(args.output)[-1]
+        print("You should use following paths in taichi_3d_gaussian_splatting config file:")
+        print(f"pointcloud-parquet-path: 'data/{name}/point_cloud.parquet'")
+        print(f"summary-writer-log-dir: data/{name}/logs")
+        print(f"output-model-dir: data/{name}/output")
+        print(f"train-dataset-json-path: 'data/{name}/train.json'")
+        print(f"val-dataset-json-path: 'data/{name}/val.json'")
+    elif args.format == 'nerfstudio':
+        print(f'output written to {args.output}')
 
 if __name__ == '__main__':
     main()
