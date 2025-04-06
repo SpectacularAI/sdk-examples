91e4219c0338b8d821a5dd5f5820cc2a9ab2d497
Paul Hine <paul.hine@gmail.com>
2025-04-06 15:14:22 +0200

    add replayRelocalize

--- python/oak/relocalize_apriltags.py
+++ python/oak/relocalize_apriltags.py
@@ -9,8 +9,8 @@ args = None
 replay = None
 
 def onAprilTagExtendedOutput(output, frameSet):
-    global args, frameTime, aprilTagFramePose
-    if (aprilTagFramePose is not None): return # already found
+    global args, frameTime, relocalizedFramePose
+    if (relocalizedFramePose is not None): return # already found
     # print(output.status)
 
     if not output.status == spectacularAI.TrackingStatus.TRACKING: return # not tracking yet
@@ -18,9 +18,24 @@ def onAprilTagExtendedOutput(output, frameSet):
     for frame in frameSet:
         if not frame.image: continue
         aprilTagFramePose = frame.cameraPose.pose.asMatrix()
+        frameTime = output.pose.time
+        print("Tracking started with April Tag at: {}\n{}".format(frameTime, aprilTagFramePose))
+        replay.close() # stop replay
+        break
+
+def onRelocalizeExtendedOutput(output, frameSet):
+    global args, frameTime, aprilTagFramePose
+    if (aprilTagFramePose is not None): return # already found
+    print(output.status)
+
+    if not output.status == spectacularAI.TrackingStatus.TRACKING: return # not tracking yet
+
+    for frame in frameSet:
+        if not frame.image: continue
+        relocalizedFramePose = frame.cameraPose.pose.asMatrix()
         # print(dir(aprilTagFramePose))
         frameTime = output.pose.time
-        print("Found April Tag at: {}\n{}".format(frameTime, aprilTagFramePose))
+        print("Tracking started with Relocalization at: {}\n{}".format(frameTime, relocalizedFramePose))
         replay.close() # stop replay
         break
 
@@ -41,6 +56,9 @@ def onAprilTagOutput(output):
     if not marker.hasPose: 
         raise Exception("Found April Tag but it's not in the tags.json file")
     
+# def onRelocalizeOutput(output):
+    # nothing here
+
 def replayAprilTags():
     global args, replay
 
@@ -55,13 +73,33 @@ def replayAprilTags():
     replay.setExtendedOutputCallback(onAprilTagExtendedOutput)
     replay.runReplay()
 
+def replayRelocalize():
+    global args, replay
+
+    print("[Replay Relocalize] {}".format(args))
+
+    configInternal = {
+        "mapLoadPath": args.dataFolder + "/tags.json",
+        "extendParameterSets" : ["relocalization"]
+    }
+
+    replay = spectacularAI.Replay(args.dataFolder, configuration=configInternal)
+    replay.setExtendedOutputCallback(onRelocalizeExtendedOutput)
+    replay.runReplay()
+
 if __name__ == '__main__':
     p = argparse.ArgumentParser(__doc__)
     p.add_argument("dataFolder", nargs="?", help="Folder containing the recorded session and tags.json", default="data")
     p.add_argument("--preview", help="Show latest primary image as a preview", action="store_true")
     args =  p.parse_args()
 
-    replayAprilTags()
+    # replayAprilTags()
+    # if not frameTime:
+    #     raise Exception("No valid frame found in April Tags replay")
+
+    replayRelocalize()
+    if not frameTime:
+        raise Exception("No valid frame found in Relocalize replay")
 
     print("done")
     
