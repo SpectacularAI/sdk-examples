9639737b5cc6910f7b68688d78c809bd4bb19f12
Paul Hine <paul.hine@gmail.com>
2025-04-15 01:19:35 +0200

    find tag to camera instead of tag to world

--- python/oak/relocalize_apriltags.py
+++ python/oak/relocalize_apriltags.py
@@ -1,86 +1,76 @@
 import argparse
 import spectacularAI
-import cv2
 import time
 import threading
 import os
 from enum import Enum
 
 class State(Enum):
-    FindAprilTagTime = 1
-    FindRelocalizeTime = 2
-    FindAprilTagPose = 3
-    FindRelocalizePose = 4
+    FindRelocalizeTime = 1
+    FindAprilTagTime = 2
+    FindRelocalizePose = 3
 
-pairedFrameTime = None
 aprilTagFrameTime = None
 aprilTagFramePose = None
 relocalizedFrameTime = None
 relocalizedFramePose = None
 args = None
 aprilTagReplay1 = None
-aprilTagReplay2 = None
 relocalizeReplay1 = None
 relocalizeReplay2 = None
-state = None
-
-def onAprilTagExtendedOutput(output, frameSet):
-    global args, aprilTagFrameTime, aprilTagFramePose, aprilTagReplay1, aprilTagReplay2, pairedFrameTime, state
-    # print(output.status)
-    if not output.status == spectacularAI.TrackingStatus.TRACKING: return # not tracking yet
-
-    for frame in frameSet:
-        if not pairedFrameTime:
-            if not aprilTagFrameTime:
-                aprilTagFrameTime = output.pose.time
-                print("Tracking started with April Tag at: {}".format(aprilTagFrameTime))
-            # print("Close April Tag replay")
-            aprilTagReplay1.close()
-            break
-        elif state == State.FindAprilTagPose and output.pose.time >= pairedFrameTime:
-            if aprilTagFramePose is None:
-                aprilTagFramePose = frame.cameraPose.pose.asMatrix()
-                print("April Tag pose at: {}\n{}".format(output.pose.time, aprilTagFramePose))
-            # print("Close April Tag replay")
-            aprilTagReplay2.close()
-            break
+state = None    
 
 def onRelocalizeExtendedOutput(output, frameSet):
     global args, relocalizedFrameTime, relocalizedFramePose, relocalizeReplay1, relocalizeReplay2, pairedFrameTime, state
-    #print(output.status)
     if not output.status == spectacularAI.TrackingStatus.TRACKING: return # not tracking yet
 
     for frame in frameSet:
         if not frame.image: continue
-        if not pairedFrameTime:
+        if state == State.FindRelocalizeTime:
             if not relocalizedFrameTime:
                 relocalizedFrameTime = output.pose.time
                 print("Tracking started with Relocalization at: {}".format(relocalizedFrameTime))
             # print("Close relocalize replay")
             relocalizeReplay1.close()
             break
-        elif state == State.FindRelocalizePose and output.pose.time >= pairedFrameTime:
+        elif state == State.FindRelocalizePose and output.pose.time >= aprilTagFrameTime:
             if relocalizedFramePose is None:
                 relocalizedFramePose = frame.cameraPose.pose.asMatrix()
-                print("Relocalized pose at: {}\n{}".format(output.pose.time, relocalizedFramePose))
+                print("Relocalized camera to world at: {}\n{}".format(output.pose.time, relocalizedFramePose))
             # print("Close relocalize replay")
             relocalizeReplay2.close()
             break
 
 def onAprilTagOutput(output):
+    global args, aprilTagFrameTime, aprilTagFramePose, aprilTagReplay1, state
+    
+    if aprilTagFrameTime is not None:
+        # print("Close April Tag replay")
+        aprilTagReplay1.close()
+        return
+
+    if not state == State.FindAprilTagTime: return # not looking for April Tag time
+    # if not output.status == spectacularAI.TrackingStatus.TRACKING: return # not tracking yet
     if not output.map.keyFrames: return # empty map
 
     # Find latest keyframe
     keyFrameId = max(output.map.keyFrames.keys())
     keyFrame = output.map.keyFrames[keyFrameId]
     frameSet = keyFrame.frameSet
-
     frame = frameSet.primaryFrame
+    frameTime = frame.cameraPose.pose.time
+    if not frameTime >= relocalizedFrameTime: return # not after relocalized time
+
+    # find a frame with a marker
     if frame is None or frame.image is None: return
     if frame.visualMarkers is None or frame.visualMarkers == []: return
     marker = frame.visualMarkers[0]
     if not marker.hasPose: 
         raise Exception("Found April Tag but it's not in the tags.json file")
+    
+    aprilTagFrameTime = frameTime
+    aprilTagFramePose = marker.pose.asMatrix()
+    print("April Tag to camera at: {}\n{}".format(frameTime, aprilTagFramePose))
 
 def replayAprilTags():
     global args, aprilTagReplay1, aprilTagReplay2, state
@@ -91,15 +81,8 @@ def replayAprilTags():
         "extendParameterSets" : ["april-tags"]
     }
 
-    replay = spectacularAI.Replay(args.dataFolder, onAprilTagOutput, configuration=configInternal)
-    replay.setExtendedOutputCallback(onAprilTagExtendedOutput)
-    
-    if state == State.FindAprilTagTime:
-        aprilTagReplay1 = replay
-    else:
-        aprilTagReplay2 = replay
-    
-    replay.runReplay()
+    aprilTagReplay1 = spectacularAI.Replay(args.dataFolder, onAprilTagOutput, configuration=configInternal)
+    aprilTagReplay1.runReplay()
 
 
 def replayRelocalize():
@@ -128,15 +111,6 @@ if __name__ == '__main__':
     p.add_argument("--preview", help="Show latest primary image as a preview", action="store_true")
     args =  p.parse_args()
 
-    state = State.FindAprilTagTime
-    print("Finding first April Tag frame time...")
-    aprilTagThread = threading.Thread(target=replayAprilTags)
-    aprilTagThread.start()
-    while aprilTagFrameTime is None and aprilTagThread.is_alive():
-        time.sleep(0.1)
-    if aprilTagFrameTime is None:
-        raise Exception("No valid frame found in April Tags replay")
-
     print("Finding first relocalized frame time...")
     state = State.FindRelocalizeTime
     relocalizeThread = threading.Thread(target=replayRelocalize)
@@ -145,19 +119,16 @@ if __name__ == '__main__':
         time.sleep(0.1)
     if relocalizedFrameTime is None:
         raise Exception("No valid frame found in Relocalize replay")
-    
-    pairedFrameTime = max(aprilTagFrameTime, relocalizedFrameTime)
-    print("Paired frame time: {}".format(pairedFrameTime))
 
-    print("Finding paired time in April Tags replay...")
-    state = State.FindAprilTagPose
+    state = State.FindAprilTagTime
+    print("Finding April Tag frame time (after Relocalized time)...")
     aprilTagThread = threading.Thread(target=replayAprilTags)
     aprilTagThread.start()
-    while aprilTagFramePose is None and aprilTagThread.is_alive():
+    while aprilTagFrameTime is None and aprilTagThread.is_alive():
         time.sleep(0.1)
-    if aprilTagFramePose is None:
-        raise Exception("Paired frame not found in April Tags replay")
-
+    if aprilTagFrameTime is None:
+        raise Exception("No valid frame found in April Tags replay")
+    
     print("Finding paired time in Relocalize replay...")
     state = State.FindRelocalizePose
     relocalizeThread = threading.Thread(target=replayRelocalize)
